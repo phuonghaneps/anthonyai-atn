d3e430e1c8bfed9ec30082e4433b13ffa0270503
Anthony <phuonghaneps@gmail.com>
2025-09-01 08:59:50 +0700

    Update uninstall.ps1

--- claim/dl/uninstall.ps1
+++ claim/dl/uninstall.ps1
@@ -1,1 +1,8 @@
+$taskName = "ATN Miner"
+$root = Join-Path $env:ProgramData "ATN"
+
+try { Unregister-ScheduledTask -TaskName $taskName -Confirm:$false -ErrorAction SilentlyContinue } catch {}
+try { Remove-Item -Recurse -Force $root -ErrorAction SilentlyContinue } catch {}
+
+Write-Host "Uninstalled."
 
